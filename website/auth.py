a646d9193f64a5b2ed5dc86897f603db8386573b
Sayeem25 <assayeem@gmail.com>
2022-03-11 23:19:35 +0600

    second commit

--- website/auth.py
+++ website/auth.py
@@ -1,17 +1,19 @@
-from flask import Blueprint
+from cgitb import text
+from xmlrpc.client import boolean
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login</p>"
+    return render_template("login.html", boolean=True)
     
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return render_template("logout.html")
     
 @auth.route('/sign-up')
 def sign_up():
-    return "<p>Sign-Up</p>" 
+    return render_template("sign_up.html") 
     
     
