5e1b95c4170f5ee0896e84b31d80321cbce3908b
Your Name <you@example.com>
2022-03-12 13:50:41 +0600

    first commit

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,17 @@
-import imp
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'SDHFGSAJFK JHSFGDK'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+    
     
     
     from .views import views
--- website/models.py
+++ website/models.py
@@ -1,1 +1,24 @@
+import email
+from email.policy import default
+from enum import unique
+from time import timezone
+from unicodedata import name
+from sqlalchemy import true
+from . import db
+from flask_login import UserMixin
+from sqlalchemy import func
 
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    data = db.Column(db.DateTime(timezone=true), default=func.now())
+    user_id = db.Column(db.Integer, db.Foreign_Key('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    name = db.Column(db.String(150))
+    notes = db.relationship('Note')
+    
