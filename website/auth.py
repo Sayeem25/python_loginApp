f053d17660a7832bb2960daa4b180b81fc29f3bf
Your Name <you@example.com>
2022-03-12 09:42:46 +0600

    third commit

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,8 @@
-from cgitb import text
-from xmlrpc.client import boolean
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login',  methods=['GET', 'POST'])
 def login():
     return render_template("login.html", boolean=True)
     
@@ -12,8 +10,25 @@ def login():
 def logout():
     return render_template("logout.html")
     
-@auth.route('/sign-up')
+@auth.route('/sign-up',  methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        name = request.form.get('name')
+        password = request.form.get('password')
+        cpassword = request.form.get('cpassword')
+        
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters.', category='error')
+        elif len(name) < 2:
+            flash('Name must be greater than 2 characters.', category='error')
+        elif password != cpassword:
+            flash('Password don\'t match', category='error')
+        elif len(password) < 7:
+            flash('Name must be at least 4 characters.', category='error')
+        else:
+            flash('Account create done!', category='success')
+        
     return render_template("sign_up.html") 
     
     
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash
 
 views = Blueprint('views', __name__)
 
