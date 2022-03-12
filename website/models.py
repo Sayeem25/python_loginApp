fece8b75d9967eb137758456cf37b3ff91ad4947
Your Name <you@example.com>
2022-03-12 14:24:50 +0600

    first commit

--- website/models.py
+++ website/models.py
@@ -13,7 +13,7 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     data = db.Column(db.DateTime(timezone=true), default=func.now())
-    user_id = db.Column(db.Integer, db.Foreign_Key('user.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
